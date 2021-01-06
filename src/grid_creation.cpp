cdad7243e5ba9f41e98cfd7229dea62ced0b4f7d
AlexisM-CPE <74047323+AlexisM-CPE@users.noreply.github.com>
2021-01-06 14:18:06 +0100

    find pos in progress

--- src/grid_creation.cpp
+++ src/grid_creation.cpp
@@ -40,7 +40,6 @@ int find_color(Mat HSV, Point2f p)
     }
     float H = H_tot/i;
     float S = S_tot/i;
-    cout<<"H : "<<H<<" S : "<<S<<" i : "<<i<<endl;
     if(i < 150)
     {
         return NO_COLOR;
@@ -75,17 +74,33 @@ void find_pos(Mat HSV, vector<Point2f> points)
   for(int i = 0; i < points.size(); i++)
   {
     int c = find_color(HSV, points[i]);
-    cout<<"color : "<<c<<endl;
-    vector<pair<float, int>> close_norm;
-    int j = 0;
-    for(auto p : points)
+    if(c != NO_COLOR)
     {
-      close_norm.push_back(make_pair(norm(p-points[i]),j));
-      j++;
+        vector<pair<float, int>> close_norm;
+        int j = 0;
+        for (auto p : points)
+        {
+            close_norm.push_back(make_pair(norm(p - points[i]), j));
+            j++;
+        }
+        sort(close_norm.begin(), close_norm.end());
+        close_norm.erase(close_norm.begin());
+        close_norm.erase(close_norm.begin() + 4, close_norm.end());
+        bool verif = true;
+        for (int k = 0; k < 2; k++)
+        {
+            for(int l = 0; l < 3; l++)
+            {
+                if (abs(close_norm[k].first - close_norm[k].first) > 7.0f)
+                    verif = false;
+                if (find_color(HSV, points[close_norm[k].second]) != c)
+                    verif = false;
+            }
+        }
+        if(verif)
+        {
+            std::cout << points[i] << std::endl;
+        }
     }
-    sort(close_norm.begin(), close_norm.end());
-    close_norm.erase(close_norm.begin());
-
-    cout<<close_norm[0].first<<" "<<close_norm[1].first<<" "<<close_norm[2].first<<" "<<close_norm[3].first<<" "<<endl;
   }
 }
