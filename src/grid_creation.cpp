602bdab9e2c909eb9704d069bfd1dac0c20c5db6
AlexisM-CPE <74047323+AlexisM-CPE@users.noreply.github.com>
2021-01-06 16:23:40 +0100

    Revert "Revert "find pos in progress""
    This reverts commit 349bde37ae80292c9ee1b960bf7b9abf2aee1919.

--- src/grid_creation.cpp
+++ src/grid_creation.cpp
@@ -71,36 +71,100 @@ int find_color(Mat HSV, Point2f p)
 
 void find_pos(Mat HSV, vector<Point2f> points)
 {
-  for(int i = 0; i < points.size(); i++)
-  {
-    int c = find_color(HSV, points[i]);
-    if(c != NO_COLOR)
+    for(int i = 0; i < points.size(); i++)
     {
-        vector<pair<float, int>> close_norm;
-        int j = 0;
-        for (auto p : points)
+        int c = find_color(HSV, points[i]);
+        if(c != NO_COLOR)
         {
-            close_norm.push_back(make_pair(norm(p - points[i]), j));
-            j++;
-        }
-        sort(close_norm.begin(), close_norm.end());
-        close_norm.erase(close_norm.begin());
-        close_norm.erase(close_norm.begin() + 4, close_norm.end());
-        bool verif = true;
-        for (int k = 0; k < 2; k++)
-        {
-            for(int l = 0; l < 3; l++)
+            vector<pair<float, int>> close_norm;
+            int j = 0;
+            for (auto p : points)
             {
-                if (abs(close_norm[k].first - close_norm[k].first) > 7.0f)
-                    verif = false;
-                if (find_color(HSV, points[close_norm[k].second]) != c)
-                    verif = false;
+                close_norm.push_back(make_pair(norm(p - points[i]), j));
+                j++;
+            }
+            sort(close_norm.begin(), close_norm.end());
+            close_norm.erase(close_norm.begin());
+            close_norm.erase(close_norm.begin() + 4, close_norm.end());
+            bool verif = true;
+            for (int k = 0; k < 3; k++)
+            {
+                for(int l = 0; l < 4; l++)
+                {
+                    if (abs(close_norm[k].first - close_norm[k].first) > 7.0f)
+                        verif = false;
+                    if (find_color(HSV, points[close_norm[k].second]) != c)
+                        verif = false;
+                }
+            }
+            if(verif)
+            {
+                for (int k = 0; k < 4; k++)
+                {
+                    Point2f dir = points[close_norm[k].second] - points[i];
+                    int loop = 0;
+                    bool stop = false;
+                    int test_fa = 0;
+                    int test_tr = 0;
+                    int color_tr = 0;
+                    Point2f pc = points[i] + dir;
+                    Point2f p_prev = points[i];
+                    vector<pair<float, int>> close_point;
+                    while(stop == false)
+                    {
+                        int color = find_color(HSV, pc);
+                        if (color == NO_COLOR)
+                        {
+                            test_fa += 1;
+                        }
+                        else if (color == color_tr)
+                        {
+                            test_tr += 1;
+                            test_fa = 0;
+                        }
+                        else if(color != c)
+                        {
+                            test_tr = 0;
+                            color_tr = color;
+                            test_fa += 1;
+                        }
+                        if (test_tr >= 2)
+                        {
+                            stop = true;
+                        }
+                        if (test_fa >= 15)
+                        {
+                            stop = true;
+                        }
+                        loop += 1;
+                        p_prev = pc;
+                        pc += dir;
+                        if ((pc.x >= 1504) || (pc.x < 0))
+                        {
+                            stop = true;
+                        }
+                        if ((pc.y >= 846) || (pc.y < 0))
+                        {
+                            stop = true;
+                        }
+                        if(stop == false)
+                        {
+                            j = 0;
+                            for (auto p : points)
+                            {
+                                close_point.push_back(make_pair(norm(p - pc), j));
+                                j++;
+                            }
+                            sort(close_point.begin(), close_point.end());
+                            if (close_point[0].first < 7.0f)
+                                pc = points[close_point[0].second];
+                            close_point.erase(close_point.begin(), close_point.end());
+                            dir = pc - p_prev;
+                        }
+                    }
+                    std::cout << points[i] << " nb loop : " << loop << " color : " << color_tr << std::endl;
+                }
             }
         }
-        if(verif)
-        {
-            std::cout << points[i] << std::endl;
-        }
     }
-  }
 }
