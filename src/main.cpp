e24221eab1414b1491d75b5a4ea27ddda920988a
Benoit Stackler <51323792+BenoitStackler@users.noreply.github.com>
2021-01-12 09:44:04 +0100

    sqdq

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "Point_Mire.hpp"
 #include "features_extraction.hpp"
 #include <opencv2/calib3d.hpp>
+#include <opencv2/features2d.hpp>
 
 #include <iostream>
 #include <cmath>
@@ -19,6 +20,7 @@ int main(int argc, char **argv)
     Mat im_gray = imread(samples::findFile("data/saliere/1.jpg"), IMREAD_GRAYSCALE);
     Mat im_gray2 = imread(samples::findFile("data/saliere/2.jpg"), IMREAD_GRAYSCALE);
     Mat im_BGR = imread(samples::findFile("data/saliere/1.jpg"), IMREAD_COLOR);
+    Mat im_BGR2 = imread(samples::findFile("data/saliere/2.jpg"), IMREAD_COLOR);
 
     if (!im_gray.data)
     {
@@ -261,8 +263,6 @@ int main(int argc, char **argv)
 
 
 
-
-
     for (int i = 0; i < image_points_output.rows - 1; i++)
     {
         auto p = image_points_output.at<Point2f>(i);
@@ -276,19 +276,116 @@ int main(int argc, char **argv)
     circle(im_BGR, Point(p.x, p.y), 1, Scalar(0, 0, 255), 5);
     imshow("Source", im_BGR);
 
-    Mat imageKey;
-    std::vector<cv::KeyPoint> feat = extract_features(im_gray, 1000);
 
-    //FAST(image, &keypointsD, threshold, true);
-    drawKeypoints(im_gray, feat, imageKey);
 
-    Ptr<StereoSGBM> BMState = cv::StereoSGBM::create(0, 8 * 16, 3, 200, 400, 0, 15, 7, 200, 2, StereoSGBM::MODE_HH);
-    BMState->compute(im_gray, im_gray2, imageKey);
-    //imageKey = (imageKey - min) * 255 / (max - min);
-    //cv::normalize(imageKey, imageKey, 0, 256, CV_MMX);
-    normalize(imageKey, imageKey, 0, 255, NORM_MINMAX, CV_8U);
-    imshow("keypoints", imageKey);
 
+    // Initiate ORB detector
+    Ptr<FeatureDetector> detector = ORB::create(2000);
+
+    std::vector<KeyPoint> key_points_1;
+    std::vector<KeyPoint> key_points_2;
+
+    Mat descriptors_1;
+    Mat descriptors_2;
+
+    // find the keypoints and descriptors with ORB
+    detector->detect(im_gray, key_points_1);
+    detector->detect(im_gray2, key_points_2);
+
+    Ptr<DescriptorExtractor> extractor = ORB::create();
+    extractor->compute(im_gray, key_points_1, descriptors_1 );
+    extractor->compute(im_gray2, key_points_2, descriptors_2 );
+
+    std::cout << key_points_1.size() << ", " << key_points_2.size() << std::endl;
+    std::cout << "_________________________________" <<std::endl;
+
+    // Flann needs the descriptors to be of type CV_32F
+    descriptors_1.convertTo(descriptors_1, CV_8UC1);
+    descriptors_2.convertTo(descriptors_2, CV_8UC1);
+
+    std::cout << descriptors_1.rows << ", " << descriptors_1.cols << std::endl;
+    std::cout << descriptors_2.rows << ", " << descriptors_2.cols << std::endl;
+    std::cout << "_________________________________" <<std::endl;
+
+    Mat im_out;
+    drawKeypoints(im_gray, key_points_1, im_out, Scalar(0,0,255));
+
+
+/*
+    FlannBasedMatcher matcher;
+    vector<DMatch> matches;
+    matcher.match( descriptors_1, descriptors_2, matches );
+
+    double max_dist = 0; double min_dist = 100;
+
+
+    //-- Quick calculation of max and min distances between keypoints
+    for( int i = 0; i < descriptors_1.rows; i++ )
+    {
+        double dist = matches[i].distance;
+        if( dist < min_dist ) min_dist = dist;
+        if( dist > max_dist ) max_dist = dist;
+    }
+
+    //-- Use only "good" matches (i.e. whose distance is less than 3*min_dist )
+    vector< DMatch > good_matches;
+
+    for( int i = 0; i < descriptors_1.rows; i++ )
+    {
+        if( matches[i].distance < 3*min_dist )
+        {
+            good_matches.push_back( matches[i]);
+        }
+    }
+    
+    vector< Point2f > obj;
+    vector< Point2f > scene;
+
+
+    for( int i = 0; i < good_matches.size(); i++ )
+    {
+        //-- Get the keypoints from the good matches
+        obj.push_back( key_points_1[ good_matches[i].queryIdx ].pt );
+        scene.push_back( key_points_1[ good_matches[i].trainIdx ].pt );
+    }
+
+
+    // Find the Homography Matrix
+    Mat H = findHomography( obj, scene, RANSAC );
+    // Use the Homography Matrix to warp the images
+    cv::Mat result;
+    warpPerspective(im_gray,result,H,Size(im_gray.cols+im_gray2.cols,im_gray.rows));
+    cv::Mat half(result,cv::Rect(0,0,im_gray2.cols,im_gray2.rows));
+    im_gray2.copyTo(half);
+    imshow( "Result", result );
+*/
+
+    // Feature matching
+	BFMatcher BF = BFMatcher(NORM_HAMMING);
+	std::vector<vector<DMatch> > matches;
+	BF.knnMatch(descriptors_1, descriptors_2, matches, 2);
+
+	std::vector<DMatch> match1;
+	std::vector<DMatch> match2;
+
+	for (int i = 0; i < matches.size(); i++)
+	{
+		match1.push_back(matches[i][0]);
+		match2.push_back(matches[i][1]);
+	}
+
+    std::cout << "Salut 1" << std::endl;
+
+    Mat img_matches1, img_matches2;
+	drawMatches(im_gray, key_points_1, im_gray2, key_points_2, match1, img_matches1);
+	drawMatches(im_gray, key_points_1, im_gray2, key_points_2, match2, img_matches2);
+
+    std::cout << "Salut 2" << std::endl;
+	imshow("test2", img_matches1);
+	imshow("test4", img_matches2);
+
+
+    imshow("out", im_out);
     // Wait and Exit
     waitKey(0);
     return 0;
