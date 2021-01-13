0aa4dae1e452533d46638a123b96daee467b5a41
Benoit Stackler <51323792+BenoitStackler@users.noreply.github.com>
2021-01-13 08:34:24 +0100

    file system

--- src/main.cpp
+++ src/main.cpp
@@ -47,6 +47,8 @@ int main(int argc, char **argv)
     Mat imageo1, imageo2;
     extract_features(im_gray_1, im_gray_2, &imageo1, &imageo2, 1000);
 
+
+
     while (true)
     {
         // Close and quit only when Escape is pressed
